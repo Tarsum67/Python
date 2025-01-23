2727edac988392406f10e2fb7563fb7928c11696
Tarsum67 <taraum@icloud.com>
2025-01-23 11:19:48 -0600

    added dataType assignment

---
+++ dataTypes.py
@@ -0,0 +1,25 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+day = 23
+print("Literal:", day)
+
+# Integer
+total_days = 342
+print("Integer:", total_days)
+
+# Float
+x = 2.14159
+print("Float:", x)
+
+# String
+name = "Travis"
+print("String:", name)
+
+# Boolean
+not_active = False
+print("Boolean:", not_active)
+
+print()
+
+print(f"Hi my name is {name}. It is January {day}th. There are {total_days} left in the year. PI is not equal to {x} so it would be {not_active}.")
