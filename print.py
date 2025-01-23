75f7ac3b398e376674905ee8d3c82c38d7bb701c
Tarsum67 <taraum@icloud.com>
2025-01-22 20:47:33 -0600

    added actives hello world and print

---
+++ print.py
@@ -0,0 +1,15 @@
+#outputs Hello World!
+print("Hello World!")
+
+#Knock Knock joke
+print("Knock, Knock.")
+print("Who's there?")
+print("Lettuce!")
+#using \n this puts the text after a new line much like the print statements above but within one print statement
+print("Lettuce Who? \nLettuce in, it's freezing out here!")
+#this separate
+print()
+#this line uses \t to tab the sentences apart
+print("Knock, Knock. \tWho's there? \tLettuce! \tLettuce Who? \tLettuce in, it's freezing out here!")
+
+
