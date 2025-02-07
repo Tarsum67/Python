15d47391393e7fbc8030b6a832be4187d528e036
Travis Routhier <tarsum67@MacBook-Pro.local>
2025-02-07 14:11:54 -0600

    finished hw 6

--- mod3/homework6.py
+++ mod3/homework6.py
@@ -1,17 +1,21 @@
 '''
 Homework6
-Name:
-github link:
+Name: "Travis Routhier"
+github link:"https://github.com/Tarsum67/Python"
 '''
 
 def div_by_seven(num):
-    # your code here
-    return 
+    for i in range(1, num + 1):
+        if i % 7 == 0:
+            print(i)
 
 def squares_of_numbers(num):
     # your code here
+    for i in range(1, num):
+        print(i**2)
     return 
 
 if __name__ == "__main__":
     import doctest
+    div_by_seven(300)
     print(doctest.testfile('doctest6.py'))
