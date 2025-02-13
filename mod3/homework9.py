5a1b5fa7e702f399debeb17124b8d812640a8f11
Travis Routhier <tarsum67@MacBookPro.hsd1.il.comcast.net>
2025-02-13 17:20:35 -0600

    finished hw 9

--- mod3/homework9.py
+++ mod3/homework9.py
@@ -6,7 +6,18 @@ github link:"https://github.com/Tarsum67/Python"
 
 def bubble_sort(lst):
     # your code here
-    return 
+    n = len(lst)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                swapped = True   
+        if not swapped:
+            break 
+    return lst                     
+     
+    
+    
 
 if __name__ == "__main__":
     import doctest
