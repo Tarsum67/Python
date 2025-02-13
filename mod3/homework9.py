996f012346014ebdde955539f2b31b68ad44dae6
Tarsum67 <taraum@icloud.com>
2025-02-13 16:16:51 -0600

    added hw 9

---
+++ mod3/homework9.py
@@ -0,0 +1,13 @@
+'''
+Homework9
+Name: "Travis Routhier"
+github link:"https://github.com/Tarsum67/Python" 
+'''
+
+def bubble_sort(lst):
+    # your code here
+    return 
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest9.py'))
