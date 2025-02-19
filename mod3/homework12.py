fe01317bc232730bec7a4bf293e0f4dd278e83b3
Tarsum67 <taraum@icloud.com>
2025-02-19 09:42:42 -0600

    finished hw 12

---
+++ mod3/homework12.py
@@ -0,0 +1,19 @@
+'''
+Homework12
+Name: "Travis Routhier"
+github link:"https://github.com/Tarsum67/Python" 
+'''
+def rectangle(side1,side2):
+    side1 * side2
+    return print(f"'The area of the rectangle is {side1 * side2} square units'")
+    
+   
+def circle(radius):
+    area = radius * radius * 3.14
+    return print(f"'The area of a circle is {area} square units'")
+   
+   
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest12.py'))
