1b3232f936d9d7eaa7a21336b39a5af14c53d9a6
Tarsum67 <taraum@icloud.com>
2025-03-17 09:41:47 -0500

    finished hw 24

---
+++ mod2/homework24.py
@@ -0,0 +1,49 @@
+'''
+Homework24
+Name: "Travis Routhier"
+github link: 
+'''
+
+
+def isBetween(word):
+    if len(word) < 8 or len(word) > 20:
+        return False
+    else:
+     return True
+
+def isUpper(word):
+    for i in word:
+        if i.isupper():
+            return True
+    return False
+
+def isLower(word):
+    for i in word:
+        if i.islower():
+            return True
+    return False
+
+def isDigit(word):
+    for i in word:
+        if i.isdigit():
+            return True
+    return False
+
+def isSpecial(word):
+    special = "!@#$%^&*()-+"
+    for i in word:
+        if i in special:
+            return True
+    return False
+
+def is_valid_password(word):
+    if isBetween(word) and isUpper(word) and isLower(word) and isDigit(word) and isSpecial(word):
+        return True
+    else:
+        return False
+   
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest24.py'))
